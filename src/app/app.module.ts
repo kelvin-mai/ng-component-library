e2d706a9e76d8c55457a4f59f307b22975ffb8d5
Kelvin Mai <kelvin.mai002@gmail.com>
2018-08-03 19:22:10 -0400

    Button works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from '../lib/public_api';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/lib/button/button.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ButtonComponent } from './button.component';
+
+@NgModule({
+  declarations: [ButtonComponent],
+  imports: [],
+  exports: [ButtonComponent],
+  providers: []
+})
+export class ButtonModule {}
